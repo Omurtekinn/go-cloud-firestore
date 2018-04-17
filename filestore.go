19e4b99f146d750ec156f788804136730a1b5ec7
Fevzi Ömür Tekin <omur1000@gmail.com>
2018-04-17 13:59:46 +0300

    create project

---
+++ filestore.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+"firebase.google.com/go"
+"google.golang.org/api/option"
+	"context"
+	"log"
+	"fmt"
+	"google.golang.org/api/iterator"
+)
+func main()  {
+
+	sa:= option.WithCredentialsFile("filestore.json")
+	app,err:=firebase.NewApp(context.Background(),nil,sa)
+	client,err:=app.Firestore(context.Background())
+	if err!=nil {
+		log.Fatalln(err)
+	}
+
+	// read cloud firestore data.
+	iter := client.Collection("Football").Documents(context.Background())
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+
+	// add to data cloud firestore.
+	_, _, err = client.Collection("Football").Add(context.Background(), map[string]interface{}{
+		"id": 3,
+		"club": "Göztepespor",
+	})
+	if err != nil {
+		log.Fatalf("Failed adding club: %v", err)
+	}
+
+	defer client.Close()
+}
+
+
+
